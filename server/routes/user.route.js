1d6fbfa3292c0d31ba96adfccd8c3e3952aa5100
mohdshebinnnc <mohammedshebinc92@gmail.com>
2025-04-19 11:40:41 +0530

    authentication

--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -36,9 +36,9 @@ userRouter.post("/register",async(req,res)=>{
             return res.status(400).json({ error: "Email already in use!" })
         }
     
-
+        const hashedPassword=await bcrypt.hash(password,10)
     
-        const newUser=new User({name,email,password,bloodType,termsAccepted})
+        const newUser=new User({name,email,password:hashedPassword,bloodType,termsAccepted})
         await newUser.save()
         res.status(201).json({message:"User regitered successfully!",userId:newUser._id})
 
@@ -62,6 +62,20 @@ userRouter.post("/login",async(req,res)=>{
             return res.status(401).json({ error: "Invalid credentials!" });
         }
 
+        const isMatch=await bcrypt.compare(password,user.password)
+        if(!isMatch){
+            return res.status(401).json({ error: "Invalid credentials!" });
+        }
+
+        const token=jwt.sign({userId:user._id,email:user.email},process.env.JWT_SECRET,{expiresIn:"7d"})
+        res.status(200).json({message:"Login Successful",token,
+            user:{
+                id: user._id,
+                name: user.name,
+                email: user.email,
+                bloodType: user.bloodType
+            }
+        })
     } catch (error) {
         console.error(error)
         res.status(500).json({ error: "Server error. Please try again later." });
