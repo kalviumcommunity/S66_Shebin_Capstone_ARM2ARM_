dbc774b2bd06d9dad22877541da52235881b7ffb
mohdshebinnnc <mohammedshebinc92@gmail.com>
2025-07-05 16:45:46 +0530

    Home page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import './App.css'
 import {BrowserRouter,Routes,Route} from 'react-router-dom'
 // import Layout from "./layout"
-import BloodRequest from "./components/BloodRequest"
 import Findblood from "./pages/findblood"
 import Donate from "./pages/donate"
 import MyDonations from "./pages/myDonations"
@@ -9,6 +8,8 @@ import MyBloodRequests from "./pages/myBloodRequests"
 import Login from "./pages/Login"
 import Register from "./pages/SignUp"
 import Home from "./pages/Home"
+import DonationCamps from "./pages/donationCamps" 
+import Profile from "./pages/Profile/Profile"
 
 
 
@@ -22,11 +23,12 @@ function App() {
             <Route path='/' element={<Home/>}/>
             <Route path='/login' element={<Login/>}/>
             <Route path='/signUp' element={<Register/>}/>
-            <Route path='/bloodRequest' element={<BloodRequest/>}/>
             <Route path='/findBlood' element={<Findblood/>}/>
             <Route path='/donate' element={<Donate/>}/>
             <Route path='/myDonations' element={<MyDonations/>}/>
             <Route path='/myRequests' element={<MyBloodRequests/>}/>
+            <Route path='/donationCamps' element={<DonationCamps/>}/>
+            <Route path='/profile' element={<Profile/>}/>
           {/* </Layout> */}
 
         </Routes>
